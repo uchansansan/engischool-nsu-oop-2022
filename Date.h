709491d4e80840dfd8517674b74b5c4211d1196a
Margarita Gnatenko <112050969+letteredMelody@users.noreply.github.com>
2022-10-13 21:36:23 +0700

    Update Date.h

--- Date.h
+++ Date.h
@@ -5,39 +5,39 @@ class Date
 {
   public:
     Date()
-		: day(1), month(1), year(0)
-	{
-	}
+      : day(1), month(1), year(0)
+      {
+      }
 
-	Date(const std::string &x)
-		: Date()
-	{
-		std::stringstream(x) >> day >> month >> year;
-	}
+    Date(const std::string &x)
+      : Date()
+      {
+        std::stringstream(x) >> day >> month >> year;
+      }
 
-	Date(const int &x, const int &y, const int &z)
-		: day(x), month(y), year(z)
-	{
-	}
+    Date(const int &x, const int &y, const int &z)
+      : day(x), month(y), year(z)
+      {
+      }
 
-	Date operator+ (const int& x) const;
-	Date operator- (const int& x) const;
-	Date operator= (const Date& x);
-	Date operator+= (const int& x);
-	Date operator-= (const int& x);
-	Date operator++ ();
-	Date operator++ (int);
-	Date operator-- ();
-	Date operator-- (int);
-	bool operator== (const Date& x);
-	bool operator!= (const Date& x);
-	bool operator< (const Date& x);
-	bool operator> (const Date& x);
-	bool operator<= (const Date& x);
-	bool operator>= (const Date& x);
-	friend std::istream& operator>> (std::istream& stream, Date& x);
-	friend std::ostream& operator<< (std::ostream& stream, const Date& x);
-	friend void PrintString (const Date& x);
+    Date operator+ (const int& x) const;
+    Date operator- (const int& x) const;
+    Date operator= (const Date& x);
+    Date operator+= (const int& x);
+    Date operator-= (const int& x);
+    Date operator++ ();
+    Date operator++ (int);
+    Date operator-- ();
+    Date operator-- (int);
+    bool operator== (const Date& x);
+    bool operator!= (const Date& x);
+    bool operator< (const Date& x);
+    bool operator> (const Date& x);
+    bool operator<= (const Date& x);
+    bool operator>= (const Date& x);
+    friend std::istream& operator>> (std::istream& stream, Date& x);
+    friend std::ostream& operator<< (std::ostream& stream, const Date& x);
+    friend void PrintString (const Date& x);
 
   private:
     int day, month, year;
